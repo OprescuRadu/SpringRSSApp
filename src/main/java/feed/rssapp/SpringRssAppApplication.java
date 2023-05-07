19b5dd92e896996621fbed0c8501768bd95f69bd
raduoprescu <r_oprescu@yahoo.com>
2023-05-07 11:41:09 +0300

    initial commit

---
+++ src/main/java/feed/rssapp/SpringRssAppApplication.java
@@ -0,0 +1,13 @@
+package feed.rssapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRssAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRssAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/feed/rssapp/SpringRssAppApplicationTests.java
@@ -0,0 +1,13 @@
+package feed.rssapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRssAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
